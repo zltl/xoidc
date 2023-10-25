ccd0d371abf2c9ea0797fc89d65a99891517e8e2
liaotonglang <liaotonglang@gmail.com>
2023-10-25 21:17:19 +0800

    update auth_request, add default value to []string values

--- gen/xoidc/public/table/table_use_schema.go
+++ gen/xoidc/public/table/table_use_schema.go
@@ -10,6 +10,7 @@ package table
 // UseSchema sets a new schema name for all generated table SQL builder types. It is recommended to invoke
 // this method only once at the beginning of the program.
 func UseSchema(schema string) {
+	AuthRequest = AuthRequest.FromSchema(schema)
 	Client = Client.FromSchema(schema)
 	ClientGrantTypes = ClientGrantTypes.FromSchema(schema)
 	ClientRedirectUris = ClientRedirectUris.FromSchema(schema)
