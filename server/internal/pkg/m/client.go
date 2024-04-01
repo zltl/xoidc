0ca47e87107023ca24f5bd796c2fab7ea4486de0
liao tonglang <liaotonglang@gmail.com>
2024-04-02 00:29:00 +0800

    tmp: move client to pkg/m

--- server/internal/pkg/m/client.go
+++ server/internal/pkg/m/client.go
@@ -2,7 +2,12 @@ package m
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/zitadel/oidc/v2/pkg/oidc"
+	"github.com/zitadel/oidc/v2/pkg/op"
+	"github.com/zitadel/oidc/v3/pkg/oidc"
+	"github.com/zitadel/oidc/v3/pkg/op"
 	"github.com/zltl/xoidc/server/internal/pkg/storage" // TODO: fix recursive import
 )
 
@@ -18,21 +23,21 @@ type ClientResponse struct {
 }
 
 type Client struct {
-	ID                             string   `json:"id"`
-	Secret                         string   `json:"secret"`
-	RedirectURIs                   []string `json:"redirect_uris"`
-	ApplicationType                int      `json:"application_type"`
-	AuthMethod                     string   `json:"auth_method"`
-	ResponseTypes                  []string `json:"response_types"`
-	GrantTypes                     []string `json:"grant_types"`
-	AccessTokenType                int      `json:"access_token_type"`
-	DevMode                        bool     `json:"dev_mode"`
-	IDTokenUserinfoClaimsAssertion bool     `json:"id_token_userinfo_claims_assertion"`
-	ClockSkew                      string   `json:"clock_skew"`
-	PostLogoutRedirectURIGlobs     []string `json:"post_logout_redirect_uri_globs"`
-	RedirectURIGlobs               []string `json:"redirect_uri_globs"`
-	UserNamespaceID                string   `json:"user_namespace_id"`
-	Name                           string   `json:"name"`
+	DID                             string   `json:"id"`
+	DSecret                         string   `json:"secret"`
+	DRedirectURIs                   []string `json:"redirect_uris"`
+	DApplicationType                int      `json:"application_type"`
+	DAuthMethod                     string   `json:"auth_method"`
+	DResponseTypes                  []string `json:"response_types"`
+	DGrantTypes                     []string `json:"grant_types"`
+	DAccessTokenType                int      `json:"access_token_type"`
+	DDevMode                        bool     `json:"dev_mode"`
+	DIDTokenUserinfoClaimsAssertion bool     `json:"id_token_userinfo_claims_assertion"`
+	DClockSkew                      string   `json:"clock_skew"`
+	DPostLogoutRedirectURIGlobs     []string `json:"post_logout_redirect_uri_globs"`
+	DRedirectURIGlobs               []string `json:"redirect_uri_globs"`
+	DUserNamespaceID                string   `json:"user_namespace_id"`
+	DName                           string   `json:"name"`
 }
 
 func toStrList[T any](ss []T) []string {
@@ -61,4 +66,116 @@ func ClientDB2View(c *storage.Client) Client {
 		UserNamespaceID:                c.UserNamespaceID(),
 		Name:                           c.Name(),
 	}
+}
+
+func (c *Client) Name() string {
+	return c.DName
+}
+
+// GetID must return the client_id
+func (c *Client) GetID() string {
+	return c.DID
+}
+
+func (c *Client) GetSecret() string {
+	return c.DSecret
+}
+
+func (c *Client) PostLogoutRedirectURIGlobs() []string {
+	return c.DPostLogoutRedirectURIGlobs
+}
+
+func (c *Client) RedirectURIGlobs() []string {
+	return c.DRedirectURIGlobs
+}
+
+func (c *Client) UserNamespaceID() string {
+	return c.DUserNamespaceID
+}
+
+// RedirectURIs must return the registered redirect_uris for Code and Implicit Flow
+func (c *Client) RedirectURIs() []string {
+	return c.DRedirectURIs
+}
+
+// PostLogoutRedirectURIs must return the registered post_logout_redirect_uris for sign-outs
+func (c *Client) PostLogoutRedirectURIs() []string {
+	// TODO
+	return []string{}
+}
+
+// ApplicationType must return the type of the client (app, native, user agent)
+func (c *Client) ApplicationType() op.ApplicationType {
+	return op.ApplicationType(c.DApplicationType)
+}
+
+// AuthMethod must return the authentication method (client_secret_basic, client_secret_post, none, private_key_jwt)
+func (c *Client) AuthMethod() oidc.AuthMethod {
+	return oidc.AuthMethod(c.DAuthMethod)
+}
+
+// ResponseTypes must return all allowed response types (code, id_token token, id_token)
+// these must match with the allowed grant types
+func (c *Client) ResponseTypes() []oidc.ResponseType {
+	return c.DResponseTypes
+}
+
+// GrantTypes must return all allowed grant types (authorization_code, refresh_token, urn:ietf:params:oauth:grant-type:jwt-bearer)
+func (c *Client) GrantTypes() []oidc.GrantType {
+	return c.DGrantTypes
+}
+
+// LoginURL will be called to redirect the user (agent) to the login UI
+// you could implement some logic here to redirect the users to different login UIs depending on the client
+func (c *Client) LoginURL(id string) string {
+	return c.loginURL(id)
+}
+
+// AccessTokenType must return the type of access token the client uses (Bearer (opaque) or JWT)
+func (c *Client) AccessTokenType() op.AccessTokenType {
+	return c.DAccessTokenType
+}
+
+// IDTokenLifetime must return the lifetime of the client's id_tokens
+func (c *Client) IDTokenLifetime() time.Duration {
+	return 1 * time.Hour
+}
+
+// DevMode enables the use of non-compliant configs such as redirect_uris (e.g. http schema for user agent client)
+func (c *Client) DevMode() bool {
+	return c.DDevMode
+}
+
+// RestrictAdditionalIdTokenScopes allows specifying which custom scopes shall be asserted into the id_token
+func (c *Client) RestrictAdditionalIdTokenScopes() func(scopes []string) []string {
+	return func(scopes []string) []string {
+		return scopes
+	}
+}
+
+// RestrictAdditionalAccessTokenScopes allows specifying which custom scopes shall be asserted into the JWT access_token
+func (c *Client) RestrictAdditionalAccessTokenScopes() func(scopes []string) []string {
+	return func(scopes []string) []string {
+		return scopes
+	}
+}
+
+// IsScopeAllowed enables Client specific custom scopes validation
+// in this example we allow the CustomScope for all clients
+func (c *Client) IsScopeAllowed(scope string) bool {
+	return scope == CustomScope
+}
+
+// IDTokenUserinfoClaimsAssertion allows specifying if claims of scope profile, email, phone and address are asserted into the id_token
+// even if an access token if issued which violates the OIDC Core spec
+// (5.4. Requesting Claims using Scope Values: https://openid.net/specs/openid-connect-core-1_0.html#ScopeClaims)
+// some clients though require that e.g. email is always in the id_token when requested even if an access_token is issued
+func (c *Client) IDTokenUserinfoClaimsAssertion() bool {
+	return c.DIDTokenUserinfoClaimsAssertion
+}
+
+// ClockSkew enables clients to instruct the OP to apply a clock skew on the various times and expirations
+// (subtract from issued_at, add to expiration, ...)
+func (c *Client) ClockSkew() time.Duration {
+	return c.DClockSkew
 }
